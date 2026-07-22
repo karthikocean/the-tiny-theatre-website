0151ba26aa1f9c351e8635fb596e2a4b4a2bc5a5
sathesh <sathesh@oceansoftteam.in>
2026-07-22 16:20:56 +0530

    feat: initialize API configuration and add decoration service module

--- src/Api/decorationapi.js
+++ src/Api/decorationapi.js
@@ -2,7 +2,7 @@ import { api } from "./api";
 
 export const getDecorations = async () => {
     try {
-        const response = await api.get("/decorations?page=1&limit=1000");
+        const response = await api.get("/decorations?page=0&limit=1000");
         return { status: true, response: response.data };
     } catch (error) {
         return { status: false, response: error.response?.data };
